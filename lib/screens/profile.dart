b789c18b6e93d9e0a37769fd51fabca8384000f0
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2024-05-27 20:39:19 +0300

    Screens <> Updated Profile Screen to Display Username, Email, and a Logout Button

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,9 +1,61 @@
 import 'package:flutter/material.dart';
-import 'screen_template.dart'; // Import screen template
+import './login.dart';
+
+// Replace with your actual navigation logic (e.g., using a navigation service)
+void navigateToLogin(BuildContext context) {
+  // Pop the current screen (profile)
+  Navigator.of(context).pop();
+  // Replace the following with your login screen navigation logic
+  // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+}
+
+void logout(BuildContext context) {
+  // Clear user data or perform logout actions (optional)
+}
 
 class ProfileScreen extends StatelessWidget {
+  final String username;
+  final String email;
+
+  const ProfileScreen({Key? key, required this.username, required this.email})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return ScreenTemplate(title: "Profile"); // Set title
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text("Profile"),
+      // ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.center, // Center content vertically
+          children: [
+            Text(
+              "Username:",
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+            Text(username),
+            SizedBox(height: 16.0), // Add spacing
+            Text(
+              "Email:",
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+            Text(email),
+            SizedBox(height: 16.0), // Add spacing
+            ElevatedButton(
+              onPressed: () {
+                logout(context); // Perform logout actions (optional)
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                );
+              },
+              child: Text("Logout"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

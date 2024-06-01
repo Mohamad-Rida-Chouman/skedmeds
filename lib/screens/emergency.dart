50a6b6d10a46ca1d906a9f7d79843ca808cba958
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2024-06-01 18:43:21 +0300

    Firebase <> Updated Emergency Screen to Retrieve Data from DB

--- lib/screens/emergency.dart
+++ lib/screens/emergency.dart
@@ -1,18 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class EmergencyScreen extends StatelessWidget {
-  final Map<String, String> emergencyNumbers = {
-    "Ambulance": "tel:112", // Replace with your local ambulance number
-    "Police": "tel:100", // Replace with your local police number
-    "Firefighters": "tel:110", // Replace with your local firefighter number
-  };
+class EmergencyScreen extends StatefulWidget {
+  @override
+  _EmergencyScreenState createState() => _EmergencyScreenState();
+}
+
+class _EmergencyScreenState extends State<EmergencyScreen> {
+  final Map<String, String> emergencyNumbers = {};
+
+  final firestoreInstance = FirebaseFirestore.instance;
+  final emergencyNumbersCollection =
+      FirebaseFirestore.instance.collection("emergency_numbers");
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() async {
+    try {
+      final querySnapshot = await emergencyNumbersCollection.get();
+      if (querySnapshot.docs.isNotEmpty) {
+        for (var doc in querySnapshot.docs) {
+          final emergencyType = doc.data()["type"]; // Assuming a "type" field
+          final phoneNumber =
+              doc.data()["phone_number"]; // Assuming a "phone_number" field
+          emergencyNumbers[emergencyType] = phoneNumber;
+        }
+      } else {
+        // Handle empty data case
+        emergencyNumbers["Numbers will be added soon!"] = "stay tuned";
+      }
+      setState(() {});
+    } catch (error) {
+      print("Error fetching data: $error");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text("Emergency Contacts"),
-      // ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -27,10 +56,14 @@ class EmergencyScreen extends StatelessWidget {
                   final emergencyType = emergencyNumbers.keys.elementAt(index);
                   final phoneNumber = emergencyNumbers.values.elementAt(index);
                   return ListTile(
-                    title: Text(emergencyType,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 16.0, fontWeight: FontWeight.bold)),
+                    title: Text(
+                      emergencyType,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                     subtitle: Text(phoneNumber, textAlign: TextAlign.center),
                   );
                 },
